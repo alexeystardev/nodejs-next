2b62d90139dc6c65323e23f06824022a4d50e5b3
Alexey <alexey.star86@gmail.com>
2021-02-06 16:06:33 +0200

    structure to start

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv').config()
+const database = require('./database')
+const path = require("path");
+const publicDirectoryPath = path.join(__dirname, "public");
+
+app.use(express.static(publicDirectoryPath));
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
+
+app.get('/', (req, res) => {
+	res.render('index')
+})
+
+database().then(info => {
+    	console.log(`Connected to ${info.host}:${info.port}/${info.name}`)
+	app.listen(process.env.PORT || `${process.env.PORT}`, () => {
+		console.log(`Server running on port: ${process.env.PORT}...`);
+	});
+}).catch(() => {
+    	console.error("Unable to connect to database")
+	process.exit(1)
+})
